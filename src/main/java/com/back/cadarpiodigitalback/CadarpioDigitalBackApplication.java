cbd697962953779441990893a13a4ec4f58abda9
PauloVictorCA <paulovictor.ca@outlook.com>
2023-05-20 19:47:32 -0300

    Criação do Project BackEnd Cadarpio Digital e incluido foodControlloer, Entities e o resources do banco de dados postgres

---
+++ src/main/java/com/back/cadarpiodigitalback/CadarpioDigitalBackApplication.java
@@ -0,0 +1,13 @@
+package com.back.cadarpiodigitalback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadarpioDigitalBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadarpioDigitalBackApplication.class, args);
+	}
+
+}
